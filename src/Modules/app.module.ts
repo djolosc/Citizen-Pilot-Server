2842f55dc5711a51cc2c3cf47e72ff6b4c52ae6f
Djordje <djsimovic@gmail.com>
2021-03-22 20:21:19 +0100

    sequelize added

--- src/Modules/app.module.ts
+++ src/Modules/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { NewsController } from '../Controllers/news.controller';
 import { NewsService } from '../Services/news.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [SequelizeModule.forRoot({
+    dialect: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'test',
+    models: [],
+  }),],
   controllers: [NewsController],
   providers: [NewsService],
 })
