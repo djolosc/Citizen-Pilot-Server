dbf45bd24611cb29321087d1557ad2e8b5bba960
Djordje <djsimovic@gmail.com>
2021-03-22 21:02:31 +0100

    not sure if its right

--- src/Models/Schemas/citizens.ts
+++ src/Models/Schemas/citizens.ts
@@ -1,31 +1,42 @@
-const Citizens = (sequelize, DataTypes) => {
-  return sequelize.define('Citizens', {
-    citizenId: {
-      type: DataTypes.STRING,
-      primaryKey: true,
-      allowNull: false,
-      unique: true,
-    },
-    citizenFName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    citizenLName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    citizenEmail: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    citizenPassword: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  }, {
-    sequelize,
-    modelName: 'Citizens'
-  })
-}
+import { Column, Model, Table } from 'sequelize-typescript';
 
-export default Citizens;
+@Table
+export class Citizens extends Model<Citizens> {
+  @Column citizenId :{
+    type: string;
+    primaryKey: boolean;
+
+  }
+
+}
+// const Citizens = (sequelize, DataTypes) => {
+//   return sequelize.define('Citizens', {
+//     citizenId: {
+//       type: DataTypes.STRING,
+//       primaryKey: true,
+//       allowNull: false,
+//       unique: true,
+//     },
+//     citizenFName: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     citizenLName: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     citizenEmail: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     citizenPassword: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//   }, {
+//     sequelize,
+//     modelName: 'Citizens'
+//   })
+// }
+
+// export default Citizens;
--- src/Modules/app.module.ts
+++ src/Modules/app.module.ts
@@ -2,17 +2,16 @@ import { Module } from '@nestjs/common';
 import { NewsController } from '../Controllers/news.controller';
 import { NewsService } from '../Services/news.service';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './core/database/database.module';
+import { NewsModule } from './news/news.module';
 
 @Module({
-  imports: [SequelizeModule.forRoot({
-    dialect: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'test',
-    models: [],
-  }),],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    NewsModule,
+],
   controllers: [NewsController],
   providers: [NewsService],
 })
